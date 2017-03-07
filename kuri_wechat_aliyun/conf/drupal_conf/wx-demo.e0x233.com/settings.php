3a015894581fa7f664074d4fa6519c4c8e8d1024
Eric Lee <ericstone.dev@gmail.com>
2017-03-07 11:57:11 +0800

    change config for aliyun wechat deployment

---
+++ kuri_wechat_aliyun/conf/drupal_conf/wx-demo.e0x233.com/settings.php
@@ -0,0 +1,28 @@
+<?php
+include dirname(__FILE__) . '/default.settings.php';
+
+$databases['default'] = [
+    'default' => [
+        'driver' => 'mysql',
+        'database' => 'sam_wechat',
+        'username' => 'sam',
+        'password' => '86d2t^xREXkZ(ad#FZvW',
+        'host' => 'rm-uf6k5oiyxefr4fpwj.mysql.rds.aliyuncs.com',
+        'prefix' => '',
+        'collation' => 'utf8mb4_general_ci',
+        'charset' => 'utf8mb4',
+    ],
+];
+
+$conf['redis_client_interface'] = 'PhpRedis';
+$conf['cache_backends'] = [
+    'sites/all/modules/contrib/redis/redis.autoload.inc',
+];
+$conf['cache_default_class'] = 'Redis_Cache';
+$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
+$conf['lock_inc'] = 'sites/all/modules/contrib/redis/redis.lock.inc';
+$conf['cache_prefix'] = 'sam_';
+$conf['redis_client_host'] = 'r-uf6b593706016944.redis.rds.aliyuncs.com';
+$conf['redis_client_port'] = '6379';
+$conf['redis_client_base'] = '0';
+$conf['redis_client_password'] = 'jPPKFoa42evB9sQcAjGy';
---
+++ kuri_wechat_aliyun/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+set -e
+
+PRO_SRV='eric@106.15.40.239'
+
+PRO_WWW='/home/eric/websites/conf'
+rsync \
+  --usermap=www-data:www-data \
+  --exclude=.DS_Store \
+  --exclude=.git/ \
+  --exclude=.gitignore \
+  --delete \
+  -arvP ./conf/ $PRO_SRV:$PRO_WWW
+
+ssh $PRO_SRV cp $PRO_WWW/drupal_conf/wx-demo.e0x233.com/settings.php /home/eric/websites/wx-demo.e0x233.com/sites/default/settings.php
+ssh $PRO_SRV cp $PRO_WWW/drupal_conf/boucheron.e0x233.com/settings.php /home/eric/websites/boucheron.e0x233.com/sites/default/settings.php
+ssh $PRO_SRV cp $PRO_WWW/drupal_conf/wx-grizzly.e0x233.com/settings.php /home/eric/websites/wx-grizzly.e0x233.com/sites/default/settings.php
